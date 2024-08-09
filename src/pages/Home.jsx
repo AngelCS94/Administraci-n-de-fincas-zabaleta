92af2653922d018b9c597118164ef2a6604ff8d4
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-08-09 18:20:43 +0200

    Mejoras en el componente Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,19 +5,21 @@ import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
-    <>
+    <div className="page-container">
       <Navbar />
-      <div className="home">
-        {posts.map((post, index) => (
-          <div key={index} className="post-wrapper">
-            <p className="post-date">
-              {new Date(post.date).toLocaleDateString()}
-            </p>
-            <BlogPost gistUrl={post.url} />
-          </div>
-        ))}
+      <div className="content-wrap">
+        <div className="home">
+          {posts.map((post, index) => (
+            <div key={index} className="post-wrapper">
+              <p className="post-date">
+                {new Date(post.date).toLocaleDateString()}
+              </p>
+              <BlogPost gistUrl={post.url} />
+            </div>
+          ))}
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
